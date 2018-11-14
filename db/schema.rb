b5acb28ee38be9ae7befbaf3fb1bb43c573c3706
Joe Fair <joe@fairanswers.com>
2018-11-14 06:10:07 -0500

    Got questions and answers to show with bootstrap.  Seeded DB for future ease of dev.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_102047) do
+ActiveRecord::Schema.define(version: 2018_11_14_102300) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "text"
+    t.boolean "correct"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,17 @@ User.create!([
   {email: "joe@fairanswers.com", encrypted_password: "$2a$11$SXepz9I7cKGzdRDuhOlV4eJF1nIm7rLuUuJP9PYtyRQpvXZZ82dUS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: nil},
   {email: "u1@d.com", encrypted_password: "$2a$11$XGz4czozSYpvCx5QUF7ZC.ITfwUxaMIv/16XUoSVr40XTmAtye/Iu", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: nil}
 ])
+Answer.create!([
+  {text: "Answer one for q1", correct: nil, question_id: 1},
+  {text: "Answer two for q1", correct: nil, question_id: 1},
+  {text: "Answer three for q1", correct: nil, question_id: 1},
+  {text: "Answer one for q1", correct: nil, question_id: 2},
+  {text: "Answer two for q2", correct: nil, question_id: 2}
+])
+Question.create!([
+  {text: "First Question", url: nil, quiz_id: 1},
+  {text: "Second Question", url: nil, quiz_id: 1}
+])
 Quiz.create!([
   {name: "First Quiz", user_id: 1}
 ])
