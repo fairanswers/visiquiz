b57e2533f7ed8e34533780301f75fc0d8fd7abd7
Joe Fair <joe@fairanswers.com>
2018-11-15 04:25:23 -0500

    Got delete question button to work

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :quiz
-  has_many :answers
+  has_many :answers, dependent: :delete_all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :quizzes
+  has_many :quizzes, dependent: :delete_all
 end
